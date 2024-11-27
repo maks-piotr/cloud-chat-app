58c5950a17a0a2c9e7e8c2f5aa9d31f62e56b06c
Maksymilian Piotrowski <maksymilian_piotrowski@wp.pl>
2024-11-27 01:18:47 +0100

    Deleted commented out code

--- fe2/chat-app/src/main.ts
+++ fe2/chat-app/src/main.ts
@@ -3,19 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { Amplify } from 'aws-amplify';
 
-// Amplify.configure({
-//   Auth: {
-//     Cognito: {
-//       userPoolId: 'us-east-1_uqVYJ45ln',
-//       userPoolClientId: '7s8cbbjpdgdnv603nerg2nf5eg',
-//       signUpVerificationMethod: 'code',
-//           loginWith: {
-//             username: true
-//           }
-      
-//     }
-//   }
-// });
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
